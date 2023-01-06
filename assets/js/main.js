bcd5e1a7ad6c309df4779341d3727d38cf9fbe65
domieee <99103235+domieee@users.noreply.github.com>
2023-01-06 12:18:52 +0100

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+function intro3(name, stadt, alter) {
+        // Hier wird unsere Funktion definiert
+        // Zusätzlich soll sie 3 Parameter entgegen nehmen
+
+    console.log("Hallo, mein Name ist " + name + ". Ich bin " + alter + " Jahre alt. Ich komme aus " + stadt + ".");  
+        // Gibt den Satz "Hallo, mein Name ist SuperCoder. Ich bin 27 Jahre alt. Ich komme aus Düsseldorf."
+        // Hierzu werden unsere übergebenen Argumente genutzt
+}   
+
+intro3("SuperCoder", "Düsseldorf", "27");
+    // Ruft unsere Funktion auf, und übergibt ihr drei Argumente
